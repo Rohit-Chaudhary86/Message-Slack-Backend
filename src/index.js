9953bc0d4f8fc3a3ae870a2b95ff41e5af7e053e
Rohit chaudhary <151979116+Rohit-Chaudhary86@users.noreply.github.com>
2025-08-29 17:53:27 +0530

    Done basick setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import { PORT } from './config/serverConfig.js';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/ping', (req, res) => {
+  return res.status(StatusCodes.OK).json({ output: 'Pong' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
